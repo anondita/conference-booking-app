792f477416a79e163c9fb8bdff50d67fd940dffa
anondita <anondita@commutatus.com>
2019-01-30 12:24:06 +0530

    redirection changed

--- app/controllers/slotuserrecords_controller.rb
+++ app/controllers/slotuserrecords_controller.rb
@@ -1,17 +1,23 @@
 class SlotuserrecordsController < ApplicationController
+	
 	def new
 		@slotuserrecord = Slotuserrecord.new
 	end
+
 	def create
 		
 		@slotuserrecord = Slotuserrecord.new(:slot_id => params[:slot_id], :user_id => params[:user_id])
+		@roomid= @slotuserrecord.slot.room.id
 		if @slotuserrecord.save
-			flash[:success]='user added'
-	else
-       flash[:danger]='Cannot add anymore users'
-       redirect_to showroom_url(@slotuserrecord.slot_id)
-   end
+			flash[:success]='user added successfully'
+			redirect_to room_slot_path(@roomid,@slotuserrecord.slot_id)
+		else
+			byebug
+       		flash[:danger]='Cannot add anymore users'
+       		redirect_to room_slot_path(@roomid,@slotuserrecord.slot_id)
+    	end
 	end
+
 	def show
 
 	end
